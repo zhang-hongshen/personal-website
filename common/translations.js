634c9f9621f02fe37bfd00ec6f6078d17a65511c
zhang-hongshen <zhanghongshen716@gmail.com>
2025-07-26 02:36:30 +0800

    20250726

---
+++ common/translations.js
@@ -0,0 +1,12 @@
+const translations = {
+    "en": {
+        "homeTab": "Home",
+        "resumeTab": "Resume",
+        "welcome": "Welcome! This is the Home tab. You can customize this content.",
+    },
+    "zh-CN": {
+        "homeTab": "首页",
+        "resumeTab": "简历",
+        "welcome": "欢迎！这是首页，你可以自定义这里的内容。",
+    }
+}; 
